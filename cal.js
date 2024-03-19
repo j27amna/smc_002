469f3a1d09bba4372af878ba4acdf220533a7d63
Amna Jehanzeb <you@example.com>
2024-03-20 00:19:20 +0500

    push origin main

---
+++ cal.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const ans = await inquirer.prompt([
+    { message: "enter a value: ", type: "number", name: "num1" },
+    { message: "enter 2nd value: ", type: 'number', name: 'num2' },
+    {
+        message: 'select your desired operator',
+        type: 'list',
+        name: 'operator',
+        choices: ['addition', 'subtraction', 'division', 'multiplication', 'exponent'],
+    },
+]);
+if (ans.operator === "addition") {
+    console.log(ans.num1 + ans.num2);
+}
+else if (ans.operator === "subtraction") {
+    console.log(ans.num1 - ans.num2);
+}
+else if (ans.operator === "division") {
+    console.log(ans.num1 / ans.num2);
+}
+else if (ans.operator === "multiplication") {
+    console.log(ans.num1 * ans.num2);
+}
+else if (ans.operator === "exponent") {
+    console.log(ans.num1 ** ans.num2);
+}
+else {
+    console.log("Error");
+}
